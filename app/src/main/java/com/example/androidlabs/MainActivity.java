e7d9af796589ade395b0b77140903f6ceb5dd5c1
Brahim Toure <Tour0038@algonquinlive.com>
2020-10-09 19:12:01 -0400

    Finished lab 3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,48 +1,52 @@
 package com.example.androidlabs;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.Toast;
+import android.widget.EditText;
 
-import com.google.android.material.snackbar.Snackbar;
 
-import static android.app.ProgressDialog.show;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences share;
+    String p ,s;
+    EditText emailAddress;
+    Button btn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lab3_linear);
+        share = this.getSharedPreferences(
+                "", Context.MODE_PRIVATE);
+       s=share.getString("ReserveName","");
 
-        setContentView(R.layout.activity_main_linear);
+        emailAddress=(EditText) findViewById(R.id.editText2);
+        emailAddress.setHint(s);
+        btn=(Button) findViewById(R.id.button2);
 
-Button btn = findViewById(R.id.button3);
-
-btn.setOnClickListener(e -> Toast.makeText(this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show());
-
-Switch s =findViewById(R.id.switch1);
-s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-    @Override
-    public void onCheckedChanged(CompoundButton cb, boolean b) {
-       Snackbar.make(s,"the switch is on",Snackbar.LENGTH_LONG ).
-               setAction("undo",click ->cb.
-                       setChecked(!b)).show();
+        btn.setOnClickListener(e->{
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+         goToProfile.putExtra("Email",emailAddress.getText());
+            startActivity(goToProfile);
+        });
 
     }
-});
-
-//
-
-
-
-
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor edit =  share .edit();
+        p=emailAddress.getText().toString();
+        edit.putString("",p);
+        edit.commit();
     }
 }
